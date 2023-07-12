a308f9bb951bd9419b78977df668065e329d7a91
Salome Essien <serenity.tonyl@gmail.com>
2023-07-12 04:36:35 -0700

    Updated

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,9 +2,19 @@
 
 def append_write(filename="", text=""):
     """
-    Append text to the  end of afile, and create if it doesnt exits.
+    Append text to the  end of afile, 
+    and create if it doesnt exits.
     """
-    with open(filename, mode="a", encoding="utf-8") as _file:
+
+    """
+    Append text to the end of file, and created if doesn't exists.
+    Args:
+      - filename: string
+      - text: string
+    """
+
+
+     with open(filename, mode="a", encoding="utf-8") as _file:
         _file.write(text)
 
     return (len(text))
