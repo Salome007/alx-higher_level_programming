1fb9b215db342d56e32bd7125778612f6b7608d0
Salome Essien <serenity.tonyl@gmail.com>
2023-07-12 04:05:53 -0700

    Python - Input/Output

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3
+"""
+This program writes in a file if doesn't exists create the file
+"""
+
 
 def write_file(filename="", text=""):
     """
-    This function writes in a file, if it doesnt exists, it creates one
+    Write in a file, if doesn't exists create the file
+    Args:
+      - filename: string
+      - text: string
     """
+
     with open(filename, mode="w", encoding="utf-8") as _file:
         _file.write(text)
 
