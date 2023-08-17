8bc6a70476e777d293aaa4ea171a9d8c5c32a0fd
Salome007 <serenity.tonyl@gmail.com>
2023-08-17 11:29:51 -0700

    sql

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,7 +1,5 @@
 -- a script that creates a table called first_table
 
-CREATE TABLE IF NOT EXISTS first_table
-(
+CREATE TABLE IF NOT EXISTS first_table(
 	id INT,
-	name VARCHAR(256),
-)
+	name VARCHAR(256));
